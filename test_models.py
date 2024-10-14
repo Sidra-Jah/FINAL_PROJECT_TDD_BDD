5d45060b73148b07979676c0e8c1734126043d63
Sidra Jahanzeb <mrssidrajahanzeb@gmail.com>
2024-10-14 11:51:40 +0200

    Added test_models.py for testing

---
+++ test_models.py
@@ -0,0 +1,22 @@
+import unittest
+from factories import models
+
+class testmodels:
+
+    def test_create(self):
+        pass
+    def test_read(Self):
+        pass
+    def test_delete(self):
+        pass
+    def test_list_all(Self):
+        pass
+    def test_find_by_name(self):
+        pass
+    def test_find_by_category(Self):
+        pass
+    def test_find_by_availability(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
